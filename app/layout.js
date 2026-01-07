494bacc510995a59d5fb7ae63a6b5de58e769909
karla-hernandez30 <karla@kch.company>
2026-01-07 17:14:58 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+
+export const metadata = {
+  title: "The Mall",
+  description: "A Y2K glitter virtual shopping mall"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
