9c571318d476064e88d4c6a29ce7e799806c7cf5
karla-hernandez30 <karla@kch.company>
2026-01-07 18:51:09 -0500

    Render dollhouse block wall

--- app/mall/page.js
+++ app/mall/page.js
@@ -2,25 +2,44 @@
 
 import Link from "next/link";
 import { stores } from "../../data/stores";
-import StoreRoom from "../../components/StoreRoom";
+
+function StoreBlock({ store }) {
+  const open = () => window.open(store.url, "_blank", "noopener,noreferrer");
+
+  return (
+    <button
+      className="block"
+      onClick={open}
+      style={{
+        gridColumn: `span ${store.col || 2}`,
+        gridRow: `span ${store.row || 1}`,
+      }}
+      aria-label={`Open ${store.name}`}
+    >
+      <div className="blockFrame">
+        <div className="blockArt" style={{ backgroundImage: `url(${store.art})` }} />
+        <div className="blockLabel">
+          <div className="blockName">{store.name}</div>
+          <div className="blockSub">{store.subtitle}</div>
+        </div>
+      </div>
+    </button>
+  );
+}
 
 export default function MallPage() {
   return (
-    <main className="towerPage">
-      <div className="towerHeader">
-        <Link href="/" className="towerNav">Exit</Link>
-        <div className="towerTitle">THE MALL</div>
-        <Link href="/food-court" className="towerNav">Food Court</Link>
+    <main className="dollhousePage">
+      <div className="dollhouseHeader">
+        <Link href="/" className="navBtn">Exit</Link>
+        <div className="title">THE MALL</div>
+        <Link href="/food-court" className="navBtn">Food Court</Link>
       </div>
 
-      <div className="towerHint">
-        Scroll the building. Tap any room to enter the real store.
-      </div>
-
-      <div className="towerGrid">
-        {stores.map((store) => (
-          <StoreRoom key={store.id} store={store} />
-        ))}
+      <div className="dollhouseWrap">
+        <div className="blockWall">
+          {stores.map((s) => <StoreBlock key={s.id} store={s} />)}
+        </div>
       </div>
     </main>
   );
