1362c2d7767fff39155f1b28b77ed1c69bb9e309
karla-hernandez30 <karla@kch.company>
2026-01-07 19:11:05 -0500

    Add store data

--- data/stores.js
+++ data/stores.js
@@ -1,37 +1,30 @@
 export const stores = [
-  // Fashion
-  { id:"hm", name:"H&M", subtitle:"fashion", url:"https://www2.hm.com/en_us/index.html", art:"/mall/hm.png", col:2, row:2 },
-  { id:"zara", name:"ZARA", subtitle:"fashion", url:"https://www.zara.com/us/", art:"/mall/zara.png", col:2, row:1 },
-  { id:"whitefox", name:"White Fox", subtitle:"boutique", url:"https://whitefoxboutique.com/", art:"/mall/whitefox.png", col:2, row:1 },
-  { id:"plt", name:"Pretty Little Things", subtitle:"party fits", url:"https://www.prettylittlething.us/", art:"/mall/plt.png", col:2, row:1 },
-  { id:"houseofcb", name:"House of Glam", subtitle:"inspired by House of CB", url:"https://app.houseofcb.com/", art:"/mall/houseofcb.png", col:2, row:1 },
-  { id:"fashionnova", name:"Nova Nights", subtitle:"inspired by Fashion Nova", url:"https://www.fashionnova.com/", art:"/mall/fashionnova.png", col:2, row:1 },
-  { id:"reformation", name:"Reform Room", subtitle:"inspired by Reformation", url:"https://www.thereformation.com/", art:"/mall/reformation.png", col:2, row:1 },
-  { id:"revolve", name:"REVOLVE", subtitle:"fashion", url:"https://www.revolve.com/", art:"/mall/revolve.png", col:2, row:1 },
-  { id:"anthro", name:"Anthro Attic", subtitle:"inspired by Anthropologie", url:"https://www.anthropologie.com/", art:"/mall/anthropologie.png", col:2, row:2 },
-  { id:"urban", name:"Urban Outfitters", subtitle:"fashion", url:"https://www.urbanoutfitters.com/", art:"/mall/urban.png", col:2, row:1 },
-  { id:"freepeople", name:"Free People", subtitle:"fashion", url:"https://www.freepeople.com/", art:"/mall/freepeople.png", col:2, row:1 },
-  { id:"princesspolly", name:"Princess Polly", subtitle:"fashion", url:"https://us.princesspolly.com/", art:"/mall/princesspolly.png", col:2, row:1 },
-  { id:"garage", name:"Garage", subtitle:"fashion", url:"https://www.garageclothing.com/", art:"/mall/garage.png", col:2, row:1 },
-  { id:"pacsun", name:"PacSun", subtitle:"fashion", url:"https://www.pacsun.com/", art:"/mall/pacsun.png", col:2, row:1 },
-
-  // Beauty
-  { id:"sephora", name:"Glow Lab", subtitle:"inspired by Sephora", url:"https://www.sephora.com/", art:"/mall/sephora.png", col:2, row:1 },
-  { id:"ulta", name:"Glam Room", subtitle:"inspired by Ulta", url:"https://www.ulta.com/", art:"/mall/ulta.png", col:2, row:1 },
-
-  // Athleisure
-  { id:"gymshark", name:"Sweat Lab", subtitle:"inspired by Gymshark", url:"https://www.gymshark.com/", art:"/mall/gymshark.png", col:2, row:1 },
-  { id:"lululemon", name:"Zen Motion", subtitle:"inspired by lululemon", url:"https://shop.lululemon.com/", art:"/mall/lululemon.png", col:2, row:1 },
-
-  // Basics / Denim
-  { id:"gap", name:"GAP", subtitle:"basics", url:"https://www.gap.com/", art:"/mall/gap.png", col:2, row:1 },
-  { id:"uniqlo", name:"UNIQLO", subtitle:"basics", url:"https://www.uniqlo.com/", art:"/mall/uniqlo.png", col:2, row:1 },
-  { id:"levi", name:"LEVI’S", subtitle:"denim", url:"https://www.levi.com/", art:"/mall/levis.png", col:2, row:1 },
-
-  // Big retail
-  { id:"target", name:"Bullseye Mart", subtitle:"inspired by Target", url:"https://www.target.com/", art:"/mall/target.png", col:2, row:1 },
-  { id:"nordstrom", name:"Nordstrom", subtitle:"department", url:"https://www.nordstrom.com/", art:"/mall/nordstrom.png", col:2, row:2 },
-
-  // Shapewear
-  { id:"skims", name:"Second Skin", subtitle:"inspired by SKIMS", url:"https://skims.com/", art:"/mall/skims.png", col:2, row:1 }
+  {
+    id: "hm",
+    name: "H&M",
+    url: "https://www2.hm.com/en_us/index.html",
+    art: "/mall/hm.png",
+    size: "xl"
+  },
+  {
+    id: "zara",
+    name: "ZARA",
+    url: "https://www.zara.com/us/",
+    art: "/mall/zara.png",
+    size: "wide"
+  },
+  {
+    id: "sephora",
+    name: "SEPHORA",
+    url: "https://www.sephora.com/",
+    art: "/mall/sephora.png",
+    size: "xl"
+  },
+  {
+    id: "skims",
+    name: "SKIMS",
+    url: "https://skims.com/",
+    art: "/mall/skims.png",
+    size: "wide"
+  }
 ];
